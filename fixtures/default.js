317815c161f28699272ca011b7361ab3bd0534b3
Jeremy <jsadwith@gmail.com>
2015-09-19 14:48:25 -0400

    First commit of subway misery

--- fixtures/default.js
+++ fixtures/default.js
@@ -4,7 +4,7 @@ var _ = require('lodash')
 
 module.exports = _.merge({
     /*
-     * Some default settings. 
+     * Some default settings.
      *
      * You can generally leave this as is for general testing purposes.
      */
@@ -26,7 +26,7 @@ module.exports = _.merge({
             id: 'local_test_step'
             , type: 'module'
             //The test runner will change YOUR_MODULE_NAME to the correct module name
-            , name: 'YOUR_MODULE_NAME'
+            , name: 'jsadwith: Subway Misery'
             , next: []
         }
     }
@@ -47,7 +47,7 @@ module.exports = _.merge({
     }
     , user: {
         /*
-         * Your dexter user settings should go in the env.js file and remain uncommitted.  
+         * Your dexter user settings should go in the env.js file and remain uncommitted.
          * For example:
          *
         profile: {
@@ -63,7 +63,7 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                subwayline: 'NQR'
             }
         }
     }
--- index.js
+++ index.js
@@ -1,13 +1,64 @@
+var http = require('http'),
+  XmlStream = require('xml-stream');
+
 module.exports = {
-    /**
-     * The main entry point for the Dexter module
-     *
-     * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
-     * @param {AppData} dexter Container for all data used in this workflow.
-     */
-    run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
-    }
+  /**
+   * The main entry point for the Dexter module
+   *
+   * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
+   * @param {AppData} dexter Container for all data used in this workflow.
+   */
+  run: function (step, dexter) {
+
+
+    var subwayline = step.input('subwayline').first(),
+      self = this;
+
+    var hostx = "web.mta.info",
+      pathx = "/status/serviceStatus.txt";
+
+    //Let the parser grab the data
+    self.getLineStatus(hostx, pathx, subwayline, function (subwaystatus) {
+
+      var response = [];
+
+      response.push({
+        subwayline: subwayline,
+        status: subwaystatus,
+        mtalink: 'http://mta.info',
+        googlemapslink: 'http://gmaps.com'
+      });
+
+
+
+      return self.complete(response);
+    });
+
+  },
+
+  getLineStatus: function (hostx, pathx, subwayline, callback) {
+
+    // Request an RSS for a Twitter stream
+    var request = http.get({
+      host: hostx,
+      path: pathx
+    }).on('response', function(response) {
+
+      var xml = new XmlStream(response);
+
+      // Pass the response as UTF-8 to XmlStream
+      response.setEncoding('utf8');
+
+      // When each item node is completely parsed, buffer its contents
+      xml.on('endElement: service > subway > line', function(subwaystatus) {
+
+        if (subwaystatus.name == subwayline) {
+          callback(subwaystatus.status);
+        }
+
+
+      });
+
+    });
+  }
 };
