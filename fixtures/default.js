e974fe94cf9ecf3f521a908f57a3bfe7bdacb30a
Daniel Ilkovich <daniel+github@betaworks.com>
2016-03-02 12:30:46 -0500

    support multiple lines

--- fixtures/default.js
+++ fixtures/default.js
@@ -73,7 +73,7 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                subwayline: 'L'
+                subwayline: 'L,S'
             }
         }
     }
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var http = require('http')
+var http      = require('http')
   , XmlStream = require('xml-stream')
+  , _         = require('lodash')
+  , q         = require('q')
 ;
 
 module.exports = {
@@ -10,37 +12,60 @@ module.exports = {
    * @param {AppData} dexter Container for all data used in this workflow.
    */
   run: function (step, dexter) {
-    var subwayline = step.input('subwayline').first()
+    var subwaylines = step.input('subwayline')
       , self       = this
       , hostx      = "web.mta.info"
       , pathx      = "/status/serviceStatus.txt"
+      , promises   = []
+      , items      = []
+      , line
     ;
 
+    if(subwaylines.length === 1) {
+        line = subwaylines.first();
+        if(line) 
+            subwaylines = line.split(',');
+    }
 
-    //Let the parser grab the data
-    self.getLineStatus(hostx, pathx, subwayline, function (subwaystatus) {
 
-      var response = []
-        , content = ''
-      ;
 
-      if (subwaystatus == 'GOOD SERVICE') {
-        content = "Today's a good day - you might get in on time. We found no MTA issues on your line. " +
-          "http://m.mta.info/mt/www.mta.info?un_jtt_v_ifnojs=Subway";
-      } else {
-        content =
-        "Yea, you're gonna be late again. We found " + subwaystatus.toLowerCase() +
-        " on your line. See if Google can save you... http://maps.google.com";
-      }
+    _.each(subwaylines, function(subwayline) {
+        //Let the parser grab the data
+        //
 
-      response.push({
-          subwayline    : subwayline,
-          subwaystatus  : subwaystatus,
-          statusmessage : content,
-      });
-
-      return self.complete(response);
+        promises.push(
+            q.ninvoke(self, 'getLineStatus', hostx, pathx, subwayline)
+        );
     });
+
+
+    q.all(promises)
+        .then(function (results) {
+            _.each(results, function(data) {
+                var response = []
+                , content  = ''
+                ;
+
+                if (data.status == 'GOOD SERVICE') {
+                    content = "Today's a good day - you might get in on time. We found no MTA issues on the "+data.line+" line. " +
+                        "http://m.mta.info/mt/www.mta.info?un_jtt_v_ifnojs=Subway";
+                } else {
+                    content =
+                    "Yea, you're gonna be late again. We found " + data.status.toLowerCase() + " on the " + data.line + " line.";
+                }
+
+                items.push({
+                    subwayline    : data.subwayline,
+                    subwaystatus  : data.status,
+                    statusmessage : content,
+                });
+            });
+
+            console.log(items.length);
+            return items;
+        })
+        .then(self.complete.bind(self, items))
+        .catch(self.fail.bind(self));
 
   },
 
@@ -49,26 +74,24 @@ module.exports = {
    *
    */
   getLineStatus: function (hostx, pathx, subwayline, callback) {
+      console.log('getLineStatus', hostx, pathx,subwayline);
 
-    // Request an RSS for a Twitter stream
-    var request = http.get({
-      host: hostx,
-      path: pathx
-    }).on('response', function(response) {
-      var xml = new XmlStream(response);
+      // Request an RSS for a Twitter stream
+      var request = http.get({
+          host: hostx,
+          path: pathx
+      }).on('response', function(response) {
+          var xml = new XmlStream(response);
 
-      // Pass the response as UTF-8 to XmlStream
-      response.setEncoding('utf8');
+          // Pass the response as UTF-8 to XmlStream
+          response.setEncoding('utf8');
 
-      // When each item node is completely parsed, buffer its contents
-      xml.on('endElement: service > subway > line', function(subwaystatus) {
-
-        console.log(subwaystatus, subwayline);
-
-        if (subwaystatus.name == subwayline) {
-            callback(subwaystatus.status);
-        }
+          // When each item node is completely parsed, buffer its contents
+          xml.on('endElement: service > subway > line', function(subwaystatus) {
+              if (subwaystatus.name == subwayline) {
+                  callback(null, { line: subwayline, status: subwaystatus.status });
+              }
+          });
       });
-    });
   }
 };
